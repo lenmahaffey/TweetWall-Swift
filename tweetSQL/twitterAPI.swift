65b50f2801e41dd61fb45ee4c6a79cc40096bbac
Len Mahaffey <len.mahaffey@gmail.com>
2017-08-13 12:49:40 -0600

    reordered lines for readability

--- tweetSQL/twitterAPI.swift
+++ tweetSQL/twitterAPI.swift
@@ -18,15 +18,15 @@ class twitterAPI {
     
     func getAuthToken() {
         let authURL = URL(string: "http://api.twitter.com/oauth2/token")
-        let authSession = URLSession(configuration: URLSessionConfiguration.default)
-        var authRequest = URLRequest(url: authURL!)
-        authRequest.httpMethod = "POST"
         let bearerTokenCredentialsData = self.bearerTokenCredentials.data(using: .utf8)
         let authString = "Basic \(bearerTokenCredentialsData!.base64EncodedString())"
+        var authRequest = URLRequest(url: authURL!)
+        authRequest.httpMethod = "POST"
         authRequest.setValue(authString , forHTTPHeaderField: "Authorization" )
         authRequest.setValue("application/x-www-form-urlencoded;charset=UTF-8", forHTTPHeaderField: "Content-Type")
         let authBody = "grant_type=client_credentials"
         authRequest.httpBody = authBody.data(using: .utf8)
+        let authSession = URLSession(configuration: URLSessionConfiguration.default)
         let authTask = authSession.dataTask(with: authRequest, completionHandler:
         {(data, response, error) in
             if let error = error {
