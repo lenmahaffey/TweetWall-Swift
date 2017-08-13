a090ea3c4d67f9250b898b17d2059633feff957d
Len Mahaffey <len.mahaffey@gmail.com>
2017-08-13 12:42:59 -0600

    added getTweets function

--- tweetSQL/twitterAPI.swift
+++ tweetSQL/twitterAPI.swift
@@ -9,12 +9,11 @@
 import Foundation
 
 class twitterAPI {
+    
     let bearerTokenCredentials = "Ou6nKK5jAYErdtHFr1esY5gZF:Cefy7q0H1fvucmaWoro7yZVj2wLjW2Am9JvQCtWzR02Z39l6WE"
-    let authString: String?
     
     init () {
-        let bearerTokenCredentialsData = self.bearerTokenCredentials.data(using: .utf8)
-        authString = "Basic \(bearerTokenCredentialsData!.base64EncodedString())"
+
     }
     
     func getAuthToken() {
@@ -22,6 +21,8 @@ class twitterAPI {
         let authSession = URLSession(configuration: URLSessionConfiguration.default)
         var authRequest = URLRequest(url: authURL!)
         authRequest.httpMethod = "POST"
+        let bearerTokenCredentialsData = self.bearerTokenCredentials.data(using: .utf8)
+        let authString = "Basic \(bearerTokenCredentialsData!.base64EncodedString())"
         authRequest.setValue(authString , forHTTPHeaderField: "Authorization" )
         authRequest.setValue("application/x-www-form-urlencoded;charset=UTF-8", forHTTPHeaderField: "Content-Type")
         let authBody = "grant_type=client_credentials"
@@ -38,11 +39,41 @@ class twitterAPI {
             }
         })
         authTask.resume()
-
     }
-//
     
-    /*
-    }*/
-
+    func getTweets (token: String, hashtag: String){
+        let apiURLComponents = NSURLComponents.init()
+        apiURLComponents.scheme = "https"
+        apiURLComponents.host = "api.twitter.com"
+        apiURLComponents.path = "/1.1/search/tweets.json"
+        apiURLComponents.query = hashtag
+        //let apiURL = apiURLComponents.url!
+        let apiURL = URL(string: "https://api.twitter.com//1.1/search/tweets.json?q=%23Denver")
+        print(apiURL!)
+        let apiSessionConfig = URLSessionConfiguration.default
+        let apiSession = URLSession(configuration: apiSessionConfig)
+        let authToken = "Bearer \(token)"
+        var apiRequest = URLRequest(url: (apiURL!))
+        apiRequest.httpMethod = "GET"
+        apiRequest.setValue(authToken , forHTTPHeaderField: "Authorization" )
+        apiRequest.setValue("application/x-www-form-urlencoded;charset=UTF-8", forHTTPHeaderField: "Content-Type")
+        let apiTask = apiSession.dataTask(with: apiRequest, completionHandler:
+        {(data, response, error) in
+            if let error = error {
+                print(error.localizedDescription)
+            }else if let data = data{
+                let JSONdata = try? JSONSerialization.jsonObject(with: data, options: []) as! [String : Any]
+                print(JSONdata!)
+                let path = "/users/bozziley/desktop/#denver.json"
+                let contents = String(data: data, encoding: String.Encoding.utf8)
+                do{
+                    try contents?.write(toFile: path, atomically: false, encoding: String.Encoding.utf8)
+                }
+                catch let error as NSError {
+                    print(error)
+                }
+            }
+        })
+        apiTask.resume()
+    }
 }
