53db3c5a46be25e27d4a8f1fb0ed593495a107c8
Len Mahaffey <len.mahaffey@gmail.com>
2017-08-31 15:59:53 -0600

    added twitterAPISeachResult with initializer

---
+++ tweetSQL/twitterAPISearchResult.swift
@@ -0,0 +1,19 @@
+//
+//  twitterAPISearchResult.swift
+//  tweetSQL
+//
+//  Created by Len Mahaffey on 8/31/17.
+//  Copyright © 2017 TLA Designs. All rights reserved.
+//
+
+import Foundation
+
+class twitterAPISearchResult {
+    
+    let data: Dictionary<String, Any>
+    
+    init (JSONTweet: Dictionary<String, Any>){
+        data = JSONTweet
+    }
+    
+}
