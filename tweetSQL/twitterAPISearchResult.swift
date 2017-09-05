679dafadcfba756acc98d7e7a571c92e19faff3e
Len Mahaffey <len.mahaffey@gmail.com>
2017-09-05 16:18:38 -0600

    added init with struct to hold data

--- tweetSQL/twitterAPISearchResult.swift
+++ tweetSQL/twitterAPISearchResult.swift
@@ -8,12 +8,36 @@
 
 import Foundation
 
+struct searchResultsMetadata {
+    let completed_in: Float?
+    let count: Int32?
+    let max_id: Int32?
+    let since_id: Int32?
+    let query: String?
+    let refresh_url: URL?
+
+    
+    init? (metaData: Dictionary<String, Any>) {
+        self.completed_in = metaData["completed_in"] as? Float
+        self.count = metaData["count"] as? Int32
+        self.max_id = metaData["max_id"] as? Int32
+        self.since_id = metaData["since_id"] as? Int32
+        self.query = metaData["query"] as? String
+        self.refresh_url = URL.init(string: "https://api.twitter.com/1.1/search/tweets.json\(String(describing: metaData["refresh_url"]))")!
+    }
+}
+
 class twitterAPISearchResult {
     
     let data: Dictionary<String, Any>
+    let results: searchResultsMetadata
+    private let db: SQliteDB
     
-    init (searchResults: Dictionary<String, Any>){
-        data = searchResults
-    }
-    
+    init (searchResults: Dictionary<String, Any>) throws {
+        self.data = searchResults
+        self.db = try SQliteDB.openDB()
+        self.results = searchResultsMetadata(metaData: self.data)!
+        }
 }
+    
+
