c3206e767cdd07186dd3014b9d15454c8d9e8385
Len Mahaffey <len.mahaffey@gmail.com>
2017-09-17 18:52:41 -0600

    Add instance variables for tweet class

--- tweetSQL/tweet.swift
+++ tweetSQL/tweet.swift
@@ -8,6 +8,25 @@
 
 import Foundation
 
+struct tweetData {
+    let source: URL
+    let truncated: Bool
+    let is_quote_status: Bool
+    let iso_language_code: String
+    let result_type: String
+    let lang: String
+    let favorited: Bool
+    let favorite_count: Int
+    let id: Int
+    let retweeted: Bool
+    let retweet_count: Int
+    let text: String
+    let created_at: Date
+    let user: NSObject
+    let in_reply_to_user_id: NSObject
+    let in_reply_to_screen_name: NSObject
+}
+
 class tweet {
     
     let data: Dictionary<String, Any>
