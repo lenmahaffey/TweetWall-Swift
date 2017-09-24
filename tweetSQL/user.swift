b163fdf1de553425f5e4d45d487e80cdfd0f5343
Len Mahaffey <len.mahaffey@gmail.com>
2017-09-24 16:11:58 -0600

    Add struct with vars

--- tweetSQL/user.swift
+++ tweetSQL/user.swift
@@ -7,3 +7,48 @@
 //
 
 import Foundation
+
+struct userData {
+    let contributors_enabled: String
+    let created_at: Date
+    let default_profile: String
+    let default_profile_image: String
+    let description: String
+    let entities: String
+    let favourites_count: String
+    let friends_count: String
+    let follow_request_sent: String
+    let followers_count: String
+    let following: String
+    let geo_enabled: String
+    let has_extended_profile: String
+    let id: String
+    let id_str: String
+    let is_translation_enabled: String
+    let is_translator: String
+    let lang: String
+    let listed_count: String
+    let location: String
+    let name: String
+    let notifications: String
+    let profile_background_color: String
+    let profile_banner_url: String
+    let profile_image_url: String
+    let profile_text_color: String
+    let profile_background_tile: String
+    let profile_use_background_image: String
+    let profile_background_image_url: String
+    let profile_background_image_url_https: String
+    let profile_image_url_https: String
+    let profile_link_color: String
+    let profile_sidebar_fill_color: String
+    let profile_sidebar_border_color: String
+    let protected: Bool
+    let screen_name: String
+    let statuses_count: String
+    let time_zone: String
+    let translator_type: String
+    let url: URL
+    let utc_offset: String
+    let verified: String
+}
