1f4eaf95e7d1fe9e55ad6ad96fc0f7eb124c7794
Len Mahaffey <len.mahaffey@gmail.com>
2017-10-04 20:46:20 -0700

    removed struct

--- tweetSQL/twitterAPISearchResult.swift
+++ tweetSQL/twitterAPISearchResult.swift
@@ -8,7 +8,10 @@
 
 import Foundation
 
-struct searchResultsMetadata {
+class twitterAPISearchResult {
+    
+    let data: Dictionary<String, Any>
+    
     let completed_in: Float?
     let count: Int32?
     let max_id: Int32?
@@ -16,26 +19,17 @@ struct searchResultsMetadata {
     let query: String?
     let refresh_url: URL?
     let statuses: Array<Any>
-
-    init (metaData: Dictionary<String, Any>, tweets: Array<Any>) {
-        self.completed_in = metaData["completed_in"] as? Float
-        self.count = metaData["count"] as? Int32
-        self.max_id = metaData["max_id"] as? Int32
-        self.since_id = metaData["since_id"] as? Int32
-        self.query = metaData["query"] as? String
-        self.refresh_url = URL.init(string: "https://api.twitter.com/1.1/search/tweets.json\(String(describing: metaData["refresh_url"]))")!
-        self.statuses = tweets
-    }
-}
-
-class twitterAPISearchResult {
-    
-    let data: Dictionary<String, Any>
-    let results: searchResultsMetadata
     
     init (searchResults: Dictionary<String, Any>, tweets: Array<Any>) {
         self.data = searchResults
-        self.results = searchResultsMetadata(metaData: searchResults, tweets: tweets)
+        
+        self.completed_in = searchResults["completed_in"] as? Float
+        self.count = searchResults["count"] as? Int32
+        self.max_id = searchResults["max_id"] as? Int32
+        self.since_id = searchResults["since_id"] as? Int32
+        self.query = searchResults["query"] as? String
+        self.refresh_url = URL.init(string: "https://api.twitter.com/1.1/search/tweets.json\(String(describing: searchResults["refresh_url"]))")!
+        self.statuses = searchResults["statuses"] as! Array
         }
 }
     
