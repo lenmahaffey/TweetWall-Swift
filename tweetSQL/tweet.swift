7e0cc1f6f1428d7588f2e2ae672c5fb8b55abce4
Len Mahaffey <len.mahaffey@gmail.com>
2017-10-10 20:32:57 -0600

    renamed class and removed unneeded var

--- tweetSQL/tweet.swift
+++ tweetSQL/tweet.swift
@@ -8,42 +8,38 @@
 
 import Foundation
 
-
-class tweet {
+class tweetClass {
     let data: Dictionary<String, Any>
-    let created_at: Date
-    let favorited: Bool
-    let favorite_count: Int
-    let id: Int
-    let is_quote_status: Bool
-    let iso_language_code: String
-    let lang: String
-    let result_type: String
-    let retweeted: Bool
-    let retweet_count: Int
-    let source: URL
-    let text: String
-    let truncated: Bool
-    //let user: String
+    
+    let created_at: String?
+    let favorited: Bool?
+    let favorite_count: Int?
+    let id: Int?
+    let lang: String?
+    let result_type: String?
+    let retweeted: Bool?
+    let retweet_count: Int?
+    let source: String?
+    let text: String?
+    let truncated: Bool?
+    let user: twitterUser
 
     
     init (JSONTweet: Dictionary<String, Any>) {
         self.data = JSONTweet
         
-        self.created_at = JSONTweet["created_at"] as! Date
-        self.favorited = JSONTweet["favorited"] as! Bool
-        self.favorite_count = JSONTweet["favorite_count"] as! Int
-        self.id = JSONTweet["id"] as! Int
-        self.is_quote_status = JSONTweet["is_quote_status"] as! Bool
-        self.iso_language_code = JSONTweet["iso_language_code"] as! String
-        self.lang = JSONTweet["lang"] as! String
-        self.result_type = JSONTweet["result_type"] as! String
-        self.retweeted = JSONTweet["retweeted"] as! Bool
-        self.retweet_count = JSONTweet["retweet_count"] as! Int
-        self.source = JSONTweet["source"] as! URL
-        self.text = JSONTweet["text"] as!String
-        self.truncated = JSONTweet["truncated"] as! Bool
-        //self.user = JSONTweet["user"] as! JSONTweet["created_at"] as! Date
+        self.created_at = JSONTweet["created_at"] as? String
+        self.favorited = JSONTweet["favorited"] as? Bool
+        self.favorite_count = JSONTweet["favorite_count"] as? Int
+        self.id = JSONTweet["id"] as? Int
+        self.lang = JSONTweet["lang"] as? String
+        self.result_type = JSONTweet["result_type"] as? String
+        self.retweeted = JSONTweet["retweeted"] as? Bool
+        self.retweet_count = JSONTweet["retweet_count"] as? Int
+        self.source = JSONTweet["source"] as? String
+        self.text = JSONTweet["text"] as? String
+        self.truncated = JSONTweet["truncated"] as? Bool
+        self.user = twitterUser.init(userJSON: JSONTweet["user"]  as! Dictionary<String,Any>)
     }
-    
+ 
 }
